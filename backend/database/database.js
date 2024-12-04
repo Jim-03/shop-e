629273a8018b9ba74d68788a837a053670ac8d0c
Jim <jimmycn03@gmail.com>
2024-12-04 17:07:23 +0300

    Connects to the database

---
+++ backend/database/database.js
@@ -0,0 +1,37 @@
+/**
+ * Module to connect to the database
+ */
+
+let database; // Sequelize object that connects to the database
+const checkVariables = require('./checkEnvironment')
+const {Sequelize} = require("sequelize");
+
+/**
+ * Attempts connecting to the database
+ * @returns {Promise<boolean>} true if successfully connected, false otherwise
+ */
+async function connectToDatabase() {
+    try {
+        // Check if environment variables are provided
+        if (!checkVariables()) {
+            console.error("Failed to connect to the database")
+            return false
+        }
+
+        // Get the database url
+        const url = checkVariables()
+
+        // Create a new sequelize object
+        database = new Sequelize(url)
+
+        // Try connecting to the database
+        await database.authenticate()
+        console.log("Connected to the database successfully")
+        return true
+    } catch (err) {
+        console.error(`An error occurred while connecting to the database:\n\t${err}`)
+        return false
+    }
+}
+module.exports = {connectToDatabase, database}
+
