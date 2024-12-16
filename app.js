e8803afc866d3b4eeb34280ce96ce1b659599c85
Jim <jimmycn03@gmail.com>
2024-12-16 13:15:56 +0300

    Added the new route

--- app.js
+++ app.js
@@ -5,10 +5,17 @@
 const express = require('express');
 const app = express();
 const { connectToDatabase } = require('./backend/database/database.js');
+
+// Routing
 app.use(express.json())
 const userRoute = require('./backend/routes/userRoute')
+const categoryRoute = require('./backend/routes/categoryRoute')
 app.use(userRoute)
+app.use(categoryRoute)
 
+/**
+ * The server's main execution point
+ */
 async function startServer () {
   // Try connecting to the database
   console.log('Connecting to database...');
