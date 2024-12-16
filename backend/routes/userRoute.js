a9404546c379ff38579e3312a872432356c5e89d
Jim <jimmycn03@gmail.com>
2024-12-16 13:15:07 +0300

    Redefined the routes and added the controller functions

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,43 +1,10 @@
 const express = require('express');
 const route = express.Router();
-const UserService = require('../services/userService');
-const userService = new UserService();
-const getStatusCode = require('./checkStatus');
+const controller = require('../controllers/userController');
 
-route.post('/account/new', (req, res) => {
-  userService.addUser(req.body)
-    .then(response => {
-      res.status(getStatusCode(response.status))
-        .json(response);
-    });
-});
-
-route.post('/account/get', (request, response) => {
-  const { username, email, phone, password } = request.body;
-  userService.getUser(username, phone, email, password)
-    .then(result => {
-      response.status(getStatusCode(result.status))
-        .json(result);
-    });
-});
-
-route.put('/account/update/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const newData = req.body;
-  userService.updateUser(id, newData)
-    .then(response => {
-      res.status(getStatusCode(response.status))
-        .json(response);
-    });
-});
-
-route.delete('/account/delete/:username', (req, res) => {
-  const { username } = req.params;
-  const { password } = req.body;
-  userService.delete(username, password)
-    .then(response => {
-      res.status(getStatusCode(response.status)).json(response);
-    });
-});
+route.post('/api/account/new', controller.addUser);
+route.post('/api/account/get', controller.getUser);
+route.put('/api/account/update/:id', controller.updateUser);
+route.delete('/api/account/delete/:username', controller.deleteUser);
 
 module.exports = route;
