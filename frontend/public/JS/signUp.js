d06d5b6c098be0206f1c00a706746e79d0c33c19
Jim <jimmycn03@gmail.com>
2024-12-23 18:37:30 +0300

    Added extra user data fields

--- frontend/public/JS/signUp.js
+++ frontend/public/JS/signUp.js
@@ -2,68 +2,109 @@
  * Adds a new user to the database
  * @returns {Promise<any|null>} a response from the database or null
  */
-async function addUser() {
-    // Fetch new user data
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("newPassword").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
+async function addUser () {
+  // Fetch new user data
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('newPassword').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const name = document.getElementById('fullName').value;
 
-    // Check if password is entered
-    if (!password || password.length !== 8) {
-        notifySignUp('red', 'Password must be 8 characters long!')
-        return
+  // Check if password is entered
+  if (!password || password.length !== 8) {
+    notifySignUp('red', 'Password must be 8 characters long!');
+    return;
+  }
+
+  // Confirm if passwords match
+  if (confirmPassword !== password) {
+    notifySignUp('red', 'Passwords don\'t match!');
+    return;
+  }
+
+  // Check if username is provided
+  if (!username || username.length === 0) {
+    notifySignUp('red', 'Enter a valid username!');
+    return;
+  }
+
+  // Check if email is provided
+  if (!email || email.length === 0) {
+    notifySignUp('red', 'Provide the email address');
+    return;
+  }
+
+  // Confirm if email is correct using regex
+  const emailRegex = /^[a-zA-Z0-9+._%+-]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$/;
+  const regex = new RegExp(emailRegex);
+  if (!regex.test(email)) {
+    notifySignUp('red', 'Enter the right email format!');
+    return;
+  }
+
+  // Confirm phone details
+  if (!phone || phone.length < 8) {
+    notifySignUp('red', 'Enter a valid phone number!');
+    return;
+  }
+  const phoneRegex = /^\+?[0-9]{1,3}[0-9]{7,12}$/;
+  const isPhoneCorrect = new RegExp(phoneRegex);
+  if (!isPhoneCorrect.test(phone)) {
+    notifySignUp('red', 'Enter a valid phone number with the country code e.g +123012345678');
+    return;
+  }
+
+  // Confirm name details
+  if (!name || name.length === 0) {
+    notifySignUp('red', 'Enter your full name!');
+    return;
+  }
+
+  try {
+    const user = {
+      username,
+      password,
+      email,
+      name,
+      phone
+    };
+    const data = await fetchUser(user);
+
+    // Notify the user on the ongoing registration
+    switch (data.status) {
+      case 'rejected':
+        notifySignUp('blue', data.message);
+        return;
+      case 'success':
+        notifySignUp('green', data.message);
+        location.replace('http://0.0.0.0:5123/user');
+        return;
+      default:
+        notifySignUp('red', data.message);
     }
-
-    // Confirm if passwords match
-    if (confirmPassword !== password) {
-        notifySignUp('red', 'Passwords don\'t match!')
-        return
-    }
-
-    // Check if username is provided
-    if (!username || username.length === 0) {
-        notifySignUp('red', 'Enter a valid username!')
-        return
-    }
-
-    try {
-        const data = await fetchUser(username, password)
-
-        // Notify the user on the ongoing registration
-        switch (data.status) {
-            case 'rejected':
-                notifySignUp('blue', data.message)
-                return
-            case 'success':
-                notifySignUp('green', data.message)
-                location.replace("http://0.0.0.0:5123/user");
-                return
-            default:
-                notifySignUp('red', data.message)
-        }
-    } catch (e) {
-        notifySignUp('red', `An error occurred while trying to communicate with the server: ${e.message}`)
-    }
+  } catch (e) {
+    notifySignUp('red', `An error occurred while trying to communicate with the server: ${e.message}`);
+  }
 }
 
 /**
  * Fetches a response status from the server
- * @param {string}username The new account's username
- * @param {string}password The new account's password
+ @param user The user's data
  * @returns {Promise<{status: string, message: string}>} A response from the database
  */
-async function fetchUser(username, password) {
-    // Add user to the api
-    const url = "http://0.0.0.0:5123/api/account/new"
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username: username, password: password})
-    })
+async function fetchUser (user) {
+  // Add user to the api
+  const url = 'http://0.0.0.0:5123/api/account/new';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  });
 
-    return await response.json();
+  return await response.json();
 }
 
 /**
