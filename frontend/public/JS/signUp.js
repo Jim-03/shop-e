04668518c2cb5e13ea7526320df6eff9e1f7985a
Jim <jimmycn03@gmail.com>
2024-12-30 12:57:27 +0300

    Updated to use a utility module

--- frontend/public/JS/signUp.js
+++ frontend/public/JS/signUp.js
@@ -1,3 +1,5 @@
+import {capitalizeName, confirmUserData, notify} from "./userUtils.js";
+
 /**
  * Adds a new user to the database
  * @returns {Promise<any|null>} a response from the database or null
@@ -11,55 +13,7 @@ async function addUser () {
   const phone = document.getElementById('phone').value.trim();
   const name = document.getElementById('fullName').value.trim();
 
-  // Check if password is entered
-  if (!password || password.length !== 8) {
-    notifySignUp('red', 'Password must be 8 characters long!');
-    return;
-  }
 
-  // Confirm if passwords match
-  if (confirmPassword !== password) {
-    notifySignUp('red', 'Passwords don\'t match!');
-    return;
-  }
-
-  // Check if username is provided
-  if (!username || username.length === 0) {
-    notifySignUp('red', 'Enter a valid username!');
-    return;
-  }
-
-  // Check if email is provided
-  if (!email || email.length === 0) {
-    notifySignUp('red', 'Provide the email address');
-    return;
-  }
-
-  // Confirm if email is correct using regex
-  const emailRegex = /^[a-zA-Z0-9+._%+-]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$/;
-  const regex = new RegExp(emailRegex);
-  if (!regex.test(email)) {
-    notifySignUp('red', 'Enter the right email format!');
-    return;
-  }
-
-  // Confirm phone details
-  if (!phone || phone.length < 8) {
-    notifySignUp('red', 'Enter a valid phone number!');
-    return;
-  }
-  const phoneRegex = /^\+?[0-9]{1,3}[0-9]{7,12}$/;
-  const isPhoneCorrect = new RegExp(phoneRegex);
-  if (!isPhoneCorrect.test(phone)) {
-    notifySignUp('red', 'Enter a valid phone number with the country code e.g +123012345678');
-    return;
-  }
-
-  // Confirm name details
-  if (!name || name.length === 0) {
-    notifySignUp('red', 'Enter your full name!');
-    return;
-  }
 
   try {
     const user = {
@@ -69,22 +23,27 @@ async function addUser () {
       name: capitalizeName(name),
       phone
     };
+
+    // Check correct user data
+    const isCorrect = confirmUserData(user, confirmPassword)
+    if (!isCorrect) return
+
     const data = await fetchUser(user);
 
     // Notify the user on the ongoing registration
     switch (data.status) {
       case 'rejected':
-        notifySignUp('blue', data.message);
+        notify('blue', data.message);
         return;
       case 'success':
-        notifySignUp('green', data.message);
+        notify('green', data.message);
         location.replace('http://0.0.0.0:5123/user');
         return;
       default:
-        notifySignUp('red', data.message);
+        notify('red', data.message);
     }
   } catch (e) {
-    notifySignUp('red', `An error occurred while trying to communicate with the server: ${e.message}`);
+    notify('red', `An error occurred while trying to communicate with the server: ${e.message}`);
   }
 }
 
@@ -107,45 +66,7 @@ async function fetchUser (user) {
   return await response.json();
 }
 
-/**
- * Notifies a user on the registration process
- * @param color The color of the message
- * @param message The message content
- */
-function notifySignUp (color, message) {
-  // Get the notification tag
-  const notification = document.getElementById('notification');
 
-  // Clear previous notification
-  notification.textContent = '';
 
-  // Set message
-  notification.textContent = message.trim();
-
-  // Set color
-  notification.style.color = color;
-
-  // Set display
-  notification.style.display = 'block';
-
-  setTimeout(() => {
-    notification.style.display = 'none';
-  }, 5000);
-}
-
-/**
- * Auto capitalizes a user's name
- * @param name The name of the user
- * @returns {string} The capitalized name
- */
-function capitalizeName (name) {
-  const nameList = name.toLowerCase().split(' ');
-  const capitalizedName = [];
-  nameList.forEach(name => {
-    capitalizedName.push(name.charAt(0).toUpperCase() + name.slice(1));
-  });
-
-  return capitalizedName.join(' ');
-}
 
 document.getElementById('signUpButton').addEventListener('click', addUser);
