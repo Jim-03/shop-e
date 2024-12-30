a214437b6124baaf1aeb0505a5a7b9d690550d69
Jim <jimmycn03@gmail.com>
2024-12-30 15:03:07 +0300

    The account UI with its create and update logic

---
+++ frontend/public/JS/account.js
@@ -0,0 +1,80 @@
+import { updatePlaceholders } from './updateAccount.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  greetUser();
+});
+
+document.getElementById('closeAbout').addEventListener('click', () => {
+  document.getElementById('aboutSection').style.display = 'none';
+});
+/**
+ * Greets the user
+ */
+function greetUser () {
+// Get the current time
+  const now = new Date();
+  const hours = now.getHours(); // Get the hour (0-23)
+
+  // Determine the greeting based on the time
+  let greeting;
+
+  if (hours < 12) {
+    greeting = 'Good Morning!';
+  } else if (hours < 16) {
+    greeting = 'Good Afternoon!';
+  } else {
+    greeting = 'Good Evening!';
+  }
+  document.getElementById('greeting').textContent = `${greeting} Jimmy`;
+}
+
+const navigationOptions = document.querySelectorAll('.options');
+navigationOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    const anchorId = option.getAttribute('id');
+    if (anchorId === 'aboutAnchor') {
+      // Get user's data from the session storage
+      const user = getUser();
+
+      // Check if user exists
+      if (user == null) return;
+
+      // Set user's data to the table display
+      displayUserData(user);
+      document.getElementById('aboutSection').style.display = 'block';
+      return;
+    }
+    if (anchorId === 'update') {
+      document.getElementById('updateSection').style.display = 'block';
+      updatePlaceholders();
+    }
+  });
+});
+
+/**
+ * Displays a user's data to the about section table
+ * @param user The user's data
+ */
+function displayUserData (user) {
+  document.getElementById('fullName').textContent = (!user.name) ? 'Unknown Customer' : user.name;
+  document.getElementById('username').textContent = (!user.username) ? 'No Username' : user.username;
+  document.getElementById('email').textContent = (!user.email) ? 'No Email Address' : user.email;
+  document.getElementById('phone').textContent = (!user.phone) ? 'No Phone Number' : user.phone;
+  document.getElementById('address').textContent = (!user.address) ? 'No Address' : user.address;
+  document.getElementById('accountType').textContent = (!user.user_type) ? 'No Account Type' : user.user_type;
+  document.getElementById('accountStatus').textContent =
+        (!user.account_status) ? 'No account Status' : user.account_status;
+}
+
+/**
+ * Retrieves a user's data from the session storage
+ * @returns {any|null} the user data, null if not found
+ */
+export function getUser () {
+  const user = JSON.parse(sessionStorage.getItem('userData'));
+  if (!user || Object.keys(user).length === 0) {
+    alert('An error has occurred while fetching your data!\nPlease login again!');
+    return null;
+  }
+  return user;
+}
---
+++ frontend/public/JS/updateAccount.js
@@ -0,0 +1,116 @@
+import { capitalizeName, confirmUserData, notify } from './userUtils.js';
+import { getUser } from './account.js';
+
+const updateButtons = document.querySelectorAll('#updateButtons button');
+updateButtons.forEach(button => {
+  button.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const type = button.getAttribute('type');
+    if (type === 'reset') {
+      document.getElementById('updateSection').style.display = 'none';
+      return;
+    }
+
+    await updateUser();
+  });
+});
+
+/**
+ * Updates the placeholders of the input fields in the update form
+ */
+export function updatePlaceholders () {
+  // Get input fields
+  const newUsername = document.getElementById('newUsername');
+  const newName = document.getElementById('newName');
+  const oldPassword = document.getElementById('oldPassword');
+  const newPassword = document.getElementById('newPassword');
+  const confirmPassword = document.getElementById('confirmPassword');
+  const newPhone = document.getElementById('newPhone');
+  const newEmail = document.getElementById('newEmail');
+
+  // Get User data
+  const user = getUser();
+
+  // Check if user exists
+  if (user === null) {
+    newUsername.setAttribute('placeholder', 'New Username');
+    newName.setAttribute('placeholder', 'Your Official Name');
+    newPhone.setAttribute('placeholder', 'New phone number');
+    newEmail.setAttribute('placeholder', 'New email address');
+  } else {
+    newUsername.value = user.username;
+    newName.value = user.name;
+    newPhone.value = user.phone;
+    newEmail.value = user.email;
+  }
+  oldPassword.setAttribute('placeholder', 'Your current password');
+  newPassword.setAttribute('placeholder', 'New Password');
+  confirmPassword.setAttribute('placeholder', 'Re-enter new password');
+}
+
+async function updateUser () {
+  const username = document.getElementById('newUsername').value.trim();
+  const password = document.getElementById('newPassword').value.trim();
+  const confirmPassword = document.getElementById('confirmPassword').value.trim();
+  const email = document.getElementById('newEmail').value.trim();
+  const phone = document.getElementById('newPhone').value.trim();
+  const name = document.getElementById('newName').value.trim();
+
+  try {
+    // Retrieve the user's data
+    const user = getUser();
+    // Update user data
+    user.username = username.toLowerCase();
+    user.password = password;
+    user.email = email;
+    user.phone = phone;
+    user.name = capitalizeName(name);
+
+    // Confirm if user data is correct
+    const isCorrect = confirmUserData(user, confirmPassword);
+    if (!isCorrect) return;
+
+    await update(user);
+  } catch (e) {
+    alert(`An error has occurred while updating the user -> ${e.message}`);
+  }
+}
+
+/**
+ * Sends a PUT request to the server
+ * @param user The new user data
+ */
+async function update (user) {
+  try {
+    const url = `http://0.0.0.0:5123/api/account/update/${user.id}`;
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+
+    const data = await response.json();
+
+    switch (data.status) {
+      case 'rejected':
+        notify('blue', data.message);
+        break;
+      case 'not_found':
+        notify('purple', data.message);
+        break;
+      case 'success':
+        notify('green', data.message);
+        setTimeout(() => {
+          document.getElementById('updateSection').style.display = 'none';
+        }, 5000);
+        break;
+      default:
+        notify('red', data.message);
+        break;
+    }
+  } catch (e) {
+    alert(`Error occurred -> ${e.message}`);
+  }
+}
